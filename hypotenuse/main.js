009000bf6d3e226c28230ad453f1d0f22a9f7b96
tyfvf <tyfvf2@gmail.com>
2022-03-29 18:20:21 -0300

    first project, a simple hypotenuse calculator

---
+++ hypotenuse/main.js
@@ -0,0 +1,14 @@
+let sideA;
+let sideB;
+let hypotenuse;
+
+document.getElementById('button').onclick = function() {
+    sideA = document.getElementById('sideA').value;
+    sideA = Number(sideA)
+    sideB = document.getElementById('sideB').value;
+    sideB = Number(sideB)
+
+    hypotenuse = Math.sqrt(Math.pow(sideA, 2) + Math.pow(sideB, 2));
+
+    document.getElementById('result').innerHTML = `The hypotenuse is equal to ` + hypotenuse;
+};
