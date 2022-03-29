1d370326fec7ab26f8b3884c53afa9cd635f7793
tyfvf <tyfvf2@gmail.com>
2022-03-29 20:02:08 -0300

    Dice project finished

---
+++ dice/main.js
@@ -0,0 +1,31 @@
+document.getElementById('d2').onclick = function() {
+    document.getElementById('labeld2').innerHTML = Math.floor(Math.random() * 2 + 1)
+};
+
+document.getElementById('d4').onclick = function() {
+    document.getElementById('labeld4').innerHTML = Math.floor(Math.random() * 4 + 1)
+};
+
+document.getElementById('d6').onclick = function() {
+    document.getElementById('labeld6').innerHTML = Math.floor(Math.random() * 6 + 1)
+};
+
+document.getElementById('d8').onclick = function() {
+    document.getElementById('labeld8').innerHTML = Math.floor(Math.random() * 8 + 1)
+};
+
+document.getElementById('d10').onclick = function() {
+    document.getElementById('labeld10').innerHTML = Math.floor(Math.random() * 10 + 1)
+};
+
+document.getElementById('d12').onclick = function() {
+    document.getElementById('labeld12').innerHTML = Math.floor(Math.random() * 12 + 1)
+};
+
+document.getElementById('d20').onclick = function() {
+    document.getElementById('labeld20').innerHTML = Math.floor(Math.random() * 20 + 1)
+};
+
+document.getElementById('d100').onclick = function() {
+    document.getElementById('labeld100').innerHTML = Math.floor(Math.random() * 100 + 1)
+};
